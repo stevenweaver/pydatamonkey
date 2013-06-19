d0381af7596b86ba958f7b653c61da571f4d69cf
Steven Weaver <weaverst@gmail.com>
2013-06-19 11:44:02 -0700

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pydatamonkey',
+      version='0.1',
+      description='Python wrapper for Datamonkey API',
+      url='http://github.com/veg/pydatamonkey',
+      author='Steven Weaver',
+      author_email='sweaver@ucsd.edu',
+      packages=['datamonkey'],
+      zip_safe=False)
+
